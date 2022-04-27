43fd45c1625e1fc3bc5d582ba09179f11c708122
Shammah Anucha <shammah.anucha@yahoo.com>
2022-04-27 06:47:03 +0200

    Add Pretty Json and colors

--- cats_fact/activity.py
+++ cats_fact/activity.py
@@ -1,9 +1,27 @@
 import requests
-
-
-def activity(response):
-    return response.json()
+import json
+from pygments import highlight
+from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.lexers.web import JsonLexer
 
 
 response = requests.get("https://www.boredapi.com/api/activity")
-print(activity(response))
+
+
+def activity(response):
+    obj = response.json()
+    # pretty print Json
+    json_formatted_str = json.dumps(obj, indent=4)
+    colorful_json = highlight(
+                              json_formatted_str,
+                              lexer=JsonLexer(),
+                              formatter=Terminal256Formatter()
+                              )
+    print(colorful_json)
+ 
+
+if __name__ == "__main__.py":
+    pass
+else:
+    activity(response)
+    
