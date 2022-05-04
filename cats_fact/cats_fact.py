7c1ede3ec32c3a1cc195e95ec8cf4f0fc82041dc
Shammah Anucha <shammah.anucha@yahoo.com>
2022-05-04 07:16:37 +0200

    Updates

--- cats_fact/cats_fact.py
+++ cats_fact/cats_fact.py
@@ -1,26 +1,33 @@
 import requests
-import json
-from pygments import highlight
-from pygments.formatters.terminal256 import Terminal256Formatter
-from pygments.lexers.web import JsonLexer
+# import json
+# from pygments import highlight
+# from pygments.formatters.terminal256 import Terminal256Formatter
+# from pygments.lexers.web import JsonLexer
+
+url = "https://catfact.ninja/fact"
 
 
-response = requests.get("https://catfact.ninja/fact")
-
-
-def cat_facts(response):
+# request mock: to mock an access to the url
+# testing the return of the function which is the fact
+def cat_facts(url):
+    response = requests.get(url)
     obj = response.json()
-    # pretty print Json
-    json_formatted_str = json.dumps(obj, indent=4)
-    colorful_json = highlight(
-                              json_formatted_str,
-                              lexer=JsonLexer(),
-                              formatter=Terminal256Formatter()
-                              )
-    print(colorful_json)
+    print(obj['fact'])
+
+    # # pretty print Json
+    # json_formatted_str = json.dumps(obj, indent=4)
+    # colorful_json = highlight(
+    #                           json_formatted_str,
+    #                           lexer=JsonLexer(),
+    #                           formatter=Terminal256Formatter()
+    #                           )
+    # status = response.status_code
+    # header = response.headers["Content-Type"]
+
+    # print(colorful_json)
+
+    # return colorful_json, status, header
 
 
-if __name__ == "__main__.py":
-    pass
-else:
-    cat_facts(response)
+if __name__ == "__main__":
+    cat_facts(url)
