07d56a71c504055ef17f88c839dee6e0750bd89f
Shammah Anucha <shammah.anucha@yahoo.com>
2022-05-04 07:50:16 +0200

    Adding colors

--- cats_fact/cats_fact.py
+++ cats_fact/cats_fact.py
@@ -1,32 +1,17 @@
+from colorama import Fore, Back, Style
 import requests
-# import json
-# from pygments import highlight
-# from pygments.formatters.terminal256 import Terminal256Formatter
-# from pygments.lexers.web import JsonLexer
+
 
 url = "https://catfact.ninja/fact"
 
 
-# request mock: to mock an access to the url
-# testing the return of the function which is the fact
 def cat_facts(url):
     response = requests.get(url)
     obj = response.json()
-    print(obj['fact'])
-
-    # # pretty print Json
-    # json_formatted_str = json.dumps(obj, indent=4)
-    # colorful_json = highlight(
-    #                           json_formatted_str,
-    #                           lexer=JsonLexer(),
-    #                           formatter=Terminal256Formatter()
-    #                           )
-    # status = response.status_code
-    # header = response.headers["Content-Type"]
-
-    # print(colorful_json)
-
-    # return colorful_json, status, header
+    print(Fore.CYAN)
+    print(Back.WHITE + obj['fact'])
+    print(Style.RESET_ALL)
+    return obj
 
 
 if __name__ == "__main__":
--- cats_fact/colors.py
+++ cats_fact/colors.py
@@ -4,3 +4,7 @@ print(Back.GREEN + 'and with a green background')
 print(Style.DIM + 'and in dim text')
 print(Style.RESET_ALL)
 print('back to normal now')
+
+
+# print('\033[31m' + 'some red text')
+# print('\033[39m')
