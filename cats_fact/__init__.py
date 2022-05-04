d449d3870b427740df416b2128c7a12d86464fd4
Shammah Anucha <shammah.anucha@yahoo.com>
2022-05-04 07:17:47 +0200

    Assert

--- cats_fact/__init__.py
+++ cats_fact/__init__.py
@@ -1,12 +1,4 @@
 __version__ = '0.1.0'
 
 
-import requests
 
-
-def cats_fact(response):
-    return response.json()
-
-
-response = requests.get("https://catfact.ninja/fact")
-print(cats_fact(response))
--- tests/test_cats_fact.py
+++ tests/test_cats_fact.py
@@ -1,5 +1,26 @@
 from cats_fact import __version__
+from cats_fact.cats_fact import cat_facts
+
+
+url = "https://catfact.ninja/fact"
+response, status, header = cat_facts(url)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_cat_facts_response():
+    assert status == 200
+
+
+def test_cat_facts_header():
+    assert header == "application/json"
+
+
+# def test_cat_facts_body():
+#     response_body = response.json()
+#     assert response_body['length'] == 61
+
+
+# mocking - to test 
