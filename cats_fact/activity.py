fd8609ce1f2c17e2536ec18eda57d741100cfc13
Shammah Anucha <shammah.anucha@yahoo.com>
2022-05-11 15:41:45 +0200

    Corrected activity

--- cats_fact/activity.py
+++ cats_fact/activity.py
@@ -1,26 +1,14 @@
 import requests
-import json
-from pygments import highlight
-from pygments.formatters.terminal256 import Terminal256Formatter
-from pygments.lexers.web import JsonLexer
+from colorama import Fore, Back, Style
 
 
-response = requests.get("https://www.boredapi.com/api/activity")
+def to_do() -> str:
+    response = requests.get("https://www.boredapi.com/api/activity")
+    obj: dict = response.json()
+
+    return obj["activity"]
 
 
-def activity(response):
-    obj = response.json()
-    # pretty print Json
-    json_formatted_str = json.dumps(obj, indent=4)
-    colorful_json = highlight(
-                              json_formatted_str,
-                              lexer=JsonLexer(),
-                              formatter=Terminal256Formatter()
-                              )
-    print(colorful_json)
-
-
-if __name__ == "__main__.py":
-    pass
-else:
-    activity(response)
+if __name__ == "__main__":
+    activity = to_do()
+    print(Back.BLACK, Fore.LIGHTYELLOW_EX, activity, Style.RESET_ALL, sep="")
